6bcd5738fecbcf0462395d4ec6a22726ad534b3f
uj0e <50102517+uj0e@users.noreply.github.com>
2024-09-19 18:11:27 +0900

    day5_js

---
+++ day5/prompt/Index.js
@@ -0,0 +1,8 @@
+// const answer = prompt("Hello World!");
+// alert(answer);
+
+const food = prompt('What did you eat on Chusuk?');
+const act = prompt('What did you do on Chusuk?');
+const feel = prompt('What did you feel on Chusuk?');
+
+console.log(`i ate ${food} on Chusuk and i did ${act} it felt ${feel}`);
