54e09742c4dd14d4f90cb48c63f4b60b3eaa6543
uj0e <dhqxlajtm040@naver.com>
2024-10-09 13:49:00 +0900

    day2

--- source/day02/src/Main.java
+++ source/day02/src/Main.java
@@ -2,6 +2,8 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("test");
         // 1. 변수 두 개를 선언해서 20과 3.0을 대입하고 두 변수의 사칙연산 결과를 정수로 출력해 보시오
 
 /*        int x = 20;
