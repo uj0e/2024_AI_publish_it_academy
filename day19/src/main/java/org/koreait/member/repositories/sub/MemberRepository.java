f57131e24989a9c55cafeabb9a59189a2aaaf88e
uj0e <50102517+uj0e@users.noreply.github.com>
2024-11-12 09:57:29 +0900

    day19

---
+++ day19/src/main/java/org/koreait/member/repositories/sub/MemberRepository.java
@@ -0,0 +1,9 @@
+package org.koreait.member.repositories.sub;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+@Repository("memRepo2")
+public class MemberRepository {
+
+}
---
+++ day19/src/test/java/org/koreait/exam02/Ex01.java
@@ -0,0 +1,30 @@
+package org.koreait.exam02;
+
+import org.junit.jupiter.api.Test;
+import org.koreait.global.configs.AppCtx4;
+import org.koreait.member.controllers.RequestJoin;
+import org.koreait.member.services.InfoService;
+import org.koreait.member.services.JoinService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Ex01 {
+    @Test
+    void test1() {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx4.class);
+
+        JoinService joinService = ctx.getBean(JoinService.class);
+        InfoService infoService = ctx.getBean(InfoService.class);
+
+        RequestJoin form = new RequestJoin();
+        form.setEmail("user01@test.org");
+        form.setPassword("12345");
+        form.setConfirmPassword("12345");
+        form.setUserName("User01");
+        form.setAgree(true);
+
+        joinService.process(form);
+        infoService.print();
+
+        ctx.close();
+    }
+}
